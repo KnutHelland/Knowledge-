7f92af337b9c27f8a939245fdb3abf22aa578aa8
Knut Helland <knutoh@gmail.com>
2011-01-08 12:28:58 +0100

    Started on a TTT grid.

--- src/KTTTGame.cpp
+++ src/KTTTGame.cpp
@@ -8,9 +8,13 @@
 
 #include <QGraphicsScene>
 #include <QPen>
+#include <iostream>
 
 #include "KTTTGame.h"
 #include "AutoScalingView.h"
+
+#define TTT_COLS 8
+#define TTT_ROWS 8
 
 
 KTTTGame::KTTTGame() {
@@ -26,6 +30,19 @@ KTTTGame::KTTTGame() {
     m_view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     m_view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     m_view->setRenderHints(QPainter::Antialiasing);
+
+    // Draw simple TTT cells. (want the margins to be 1 cell width)
+    qreal cellWidth = 1.0 / (TTT_COLS + 2);
+    qreal cellHeight = 1.0 / (TTT_ROWS + 2);
+
+    for (int i = 2; i < (TTT_COLS+1); i++) {
+	m_scene->addLine((qreal)i * cellWidth, cellWidth, (qreal)i * cellWidth, 1-cellWidth);
+	std::cout << i * cellWidth << std::endl;
+    }
+
+    for (int i = 2; i < (TTT_ROWS+1); i++) {
+	m_scene->addLine(cellHeight, (qreal)i * cellHeight, 1-cellHeight, (qreal)i * cellHeight);
+    }
 }
 
 
